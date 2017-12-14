f67eafc9fbb50076944a707402206189edf7cfb3
Rashmi Arora <abhishek9982@gmail.com>
2017-12-14 23:27:42 +0100

    AA-1023: Create Baseline for Administration Application in GITHUB

---
+++ administration/baseline/tables/table.ad_applications.0001.create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "AD_APPLICATIONS" (
+    "APPLICATION_ID"        NUMBER,
+    "APPLICATION_NAME"      VARCHAR2(150 BYTE),
+    "APPLICATION"           VARCHAR2(150 BYTE),
+    "APEX_APPLICATION_ID"   NUMBER,
+    "ENABLED"               VARCHAR2(1 BYTE),
+    "DATE_FROM"             DATE,
+    "DATE_TO"               DATE,
+    "CREATED_BY"            NUMBER,
+    "CREATED_ON"            DATE,
+    "UPDATED_BY"            NUMBER,
+    "UPDATED_ON"            DATE
+);
